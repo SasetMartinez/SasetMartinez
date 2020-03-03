2e8c506467a4acd567b845791ebbbdedbeb088fa
SasetMartinez <37758037+SasetMartinez@users.noreply.github.com>
2020-03-02 19:14:34 -0600

    Create Activity01.py

---
+++ Activity01.py
@@ -0,0 +1,19 @@
+DIVIDER="--------------------"
+print(DIVIDER)
+
+id="SA3001MAPS"
+print("id:{}".format(id))
+print(DIVIDER)
+
+f_name="Saset"
+l_name="Martinez"
+n_name="Zapatito"
+age=18
+
+print("Name: {},{}".format(f_name,l_name))
+print("Nickname: {}".format(n_name))
+print("Age: {} years".format(age))
+print(DIVIDER)
+
+famous=True
+print("Famous Person: {}".format(famous))
